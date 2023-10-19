166809158c8a8b78734c0c8f43713471aaed12cc
5Yago <senaiyago44@gmail.com>
2023-10-19 10:43:41 -0300

    fase_2_imagens

--- index.php
+++ index.php
@@ -2,8 +2,43 @@
     include_once("templates/header.php");
 ?>
 
-<h1>Olá mundo!! Bem-vindo ao PHP!!</h1>
+<!-- <h1>Olá mundo!! Bem-vindo ao PHP!!</h1> -->
 
+<?php
+
+    // var_dump($posts);
+    // print_r($posts);
+    // echo"<br><br>";
+
+?>
+
+<main>
+    <div id="title-container">
+        <h1>Code Blog</h1>
+        <p>O se seu blog de programação</p>
+
+    </div>
+    <div id="posts-container">
+        <?php foreach($posts as $post) : ?>
+            <div class="post-box">
+                <img src="<?=$BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+
+                <h2 class="post-title"><a href="<?$BASE_URL ?>post.php?id=<?= $post ['id']?>"><?= $post ['title'] ?> </a></h2>
+
+                <p class="post-description"><?= $post['description'] ?>
+
+                <div class="tags-container">
+                <?php foreach($posts['tags'] as $tag) : ?>
+                    <a href="#"<?=$tag ?>></a>
+                    <?php endforeach; ?>
+
+                </div>
+                
+
+            <?php endforeach; ?>
+            </div>
+        </div>
+</main>
 <?php
     include_once("templates/footer.php");
 ?>
